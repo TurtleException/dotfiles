dddee5c31777ed21d369c1c3ed790032cca2afc4
TurtleException <turtle.exception@eldritch.de>
2024-10-13 17:41:15 +0200

    Fix literal use of "hostname" in motd.sh

--- motd.sh
+++ motd.sh
@@ -3,4 +3,4 @@
 # Displays an ASCII turtle with the hostname of the system as a banner
 # This is intended to be used as an motd
 
-paste <(cat res/turtle) <(figlet hostname)
+paste <(cat res/turtle) <(figlet $(hostname))
