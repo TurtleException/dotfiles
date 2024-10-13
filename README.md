580d371f8ca1e47c47664ab407a6af35491b2410
TurtleException <turtle.exception@eldritch.de>
2024-10-13 18:27:09 +0200

    Implement convenience script

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ This directory contains my commonly used dotfiles - including aliases, bash conf
 ```bash
 git clone https://github.com/TurtleException/dotfiles ~/.dotfiles && bash ~/.dotfiles/install.sh
 ```
+or you can use the convenience-script:
+```bash
+curl -fsSL https://dotfiles.turtleboi.de | bash
+```
---
+++ convenience_script.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Asks a Yes/No question and prompts the user (defaults to yes if not input is supplied)
+function ask() {
+  read -p "$1 (Y/n): " response
+  test -z "$response" \
+    || [ "$response" = "y" ] \
+    || [ "$response" = "Y" ] \
+    || [ "$response" = "yes" ] \
+    || [ "$response" = "Yes" ] \
+    || [ "$response" = "YES" ]
+}
+
+function main() {
+	if ! command -v git &>/dev/null; then
+		echo "Git is not installed!"
+
+		# Attempt to install via APT
+		if command -v apt &>/dev/null; then
+			if ask "  Attempt to install via APT?"; then
+				sudo apt install git
+			else
+				exit 1
+			fi
+		elif command -v dnf &>/dev/null; then
+			if ask "  Attempt to install via DNF?"; then
+				sudo dnf install git
+			else
+				exit 1
+			fi
+		elif command -v yum &>/dev/null; then
+			if ask "  Attempt to install via YUM?"; then
+				sudo yum install git
+			else
+				exit 1
+			fi
+		elif command -v brew &>/dev/null; then
+			if ask "  Attempt to install via Homebrew?"; then
+				brew install git
+			else
+				exit 1
+			fi
+		else
+			echo "  Failed at attempting auto-installation"
+			exit 2
+		fi
+	fi
+
+	git clone https://github.com/TurtleException/dotfiles ~/.dotfiles && bash ~/.dotfiles/install.sh
+}
+
+# Only invoke at the end to prevent partial transmission of this script
+main
