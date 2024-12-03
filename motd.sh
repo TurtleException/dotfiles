5cc4a697513e1a9483522c97fc75ef831bc11e08
TurtleException <dev@turtle-mail.de>
2024-12-03 01:08:36 +0100

    Update motd.sh | Use pretty hostname if available

--- motd.sh
+++ motd.sh
@@ -5,4 +5,9 @@
 
 dotfiles_dir=$(dirname "$0")
 
-paste <(cat "$dotfiles_dir"/res/turtle) <(figlet $(hostname))
+hostname=$(hostnamectl hostname --pretty)
+if [ "$hostname" == "" ]; then
+	hostname=$(hostnamectl hostname)
+fi
+
+paste <(cat "$dotfiles_dir"/res/turtle) <(figlet "$hostname")
